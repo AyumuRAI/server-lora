5057a08d08383fed6acaf3ba83ddb0141c9ed02b
Ayumu-RAI <rai.ayumucueva@gmail.com>
2025-09-14 23:31:14 +0800

    A query for MPIN 6 code verification for SMS

--- src/graphql/resolvers/account.resolver.ts
+++ src/graphql/resolvers/account.resolver.ts
@@ -1,10 +1,59 @@
 import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+import twilio from "twilio";
+
+dotenv.config();
 
 const prisma = new PrismaClient();
 
+// TWILIO
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const serviceSID = process.env.TWILIO_SERVICE_SID!;
+const client = twilio(accountSid, authToken);
+
 export const resolvers = {
   Query: {
-    _empty: () => "",
+    sendMPIN: async (_: any, args: { phone: string }) => {
+      try {
+        const verification = await client.verify.v2
+          .services(serviceSID)
+          .verifications.create({
+            to: args.phone,
+            channel: "sms",
+          });
+
+        return {
+          success: verification.status === "pending",
+          message: "MPIN sent successfully",
+        };
+      } catch (err: any) {
+        return {
+          success: false,
+          message: err.message,
+        };
+      }
+    },
+    verifyMPIN: async (_: any, args: { phone: string; code: string }) => {
+      try {
+        const verification = await client.verify.v2
+          .services(serviceSID)
+          .verificationChecks.create({
+            to: args.phone,
+            code: args.code,
+          });
+
+        return {
+          success: verification.status === "approved",
+          message: "MPIN verified successfully",
+        };
+      } catch (err: any) {
+        return {
+          success: false,
+          message: err.message,
+        };
+      }
+    },
   },
   Mutation: {
     _empty: () => "",
