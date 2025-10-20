02657af7c025ee5f456a00038b8bdd4cfbdae170
Ayumu-RAI <rai.ayumucueva@gmail.com>
2025-10-20 17:53:37 +0800

    Added a mock payment method for EWallet only GCASH and PAYMAYA, this is not the final logic. This is a mock method for testing purposes only

--- src/graphql/resolvers/payment.resolver.ts
+++ src/graphql/resolvers/payment.resolver.ts
@@ -1,4 +1,5 @@
-import { EWallet, VirtualAcc } from "@lib/xenditClient";
+import { PaymentRequestParameters, PaymentRequest } from "xendit-node/payment_request/models";
+import { xenditPaymentRequestClient } from "@lib/xenditClient";
 import { Context } from "@lib/context";
 
 export const resolvers = {
@@ -8,8 +9,49 @@ export const resolvers = {
     }
   },
   Mutation: {
-    _any: async (_any: any, args: {}, context: Context) => {
-      return "";
+    createPayment: async (_any: any, args: { method: string, amount: number }, context: Context) => {
+        try {
+          
+          const selectedMethod = args.method.toUpperCase();
+          const paymentAmount = args.amount;
+
+          // EWallet
+          if (["GCASH", "PAYMAYA"].includes(selectedMethod)) {
+            const data: PaymentRequestParameters = {
+              "amount" : paymentAmount,
+              "paymentMethod" : {
+                "ewallet" : {
+                  "channelProperties" : {
+                    "successReturnUrl" : "https://google.com",
+                    "failureReturnUrl" : "https://google.com"
+                  },
+                  "channelCode" : selectedMethod as "GCASH" | "PAYMAYA"
+                },
+                "reusability" : "ONE_TIME_USE",
+                "type" : "EWALLET"
+              },
+              "currency" : "PHP",
+              "referenceId" : "example-ref-1234"
+            };
+
+            const response: PaymentRequest = await xenditPaymentRequestClient.createPaymentRequest({
+              data
+            });
+
+            console.log(response);
+          };
+
+          return {
+            success: true,
+            message: "Payment created successfully"
+          };
+
+        } catch(err: any) {
+          return {
+            success: false,
+            message: JSON.stringify(err)
+          };
+        };
     }
   }
 };
