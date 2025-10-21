055ed0e6fbcd0221a349011ee2267f78a61bd202
Ayumu-RAI <rai.ayumucueva@gmail.com>
2025-10-21 18:08:35 +0800

    Added the payment method logic for banks payment action, but this is not the final version. This is still under development and testing

--- src/graphql/resolvers/payment.resolver.ts
+++ src/graphql/resolvers/payment.resolver.ts
@@ -1,6 +1,7 @@
 import { PaymentRequestParameters, PaymentRequest } from "xendit-node/payment_request/models";
-import { xenditPaymentRequestClient } from "@lib/xenditClient";
+import { xenditPaymentRequestClient, xenditPaymentMethodClient } from "@lib/xenditClient";
 import { Context } from "@lib/context";
+import { User } from "@lib/types";
 
 export const resolvers = {
   Query: {
@@ -11,12 +12,38 @@ export const resolvers = {
   Mutation: {
     createPayment: async (_any: any, args: { method: string, amount: number }, context: Context) => {
         try {
+          if (!context.user) {
+            return {
+              success: false,
+              message: "Unauthorized"
+            };
+          };
+
+          /*
+            Payment options:
+            - GCASH
+            - PAYMAYA
+
+            - BPI
+            - BDO
+            - METROBANK
+            - LANDBANK
+            - UNIONBANK
+            
+            - VISA
+            - MASTERCARD
+          */
+
+          const user = context.user as User;
+          const EWALLET = ["GCASH", "PAYMAYA"];
+          const BANK = ["BPI", "BDO", "METROBANK", "LANDBANK", "UNIONBANK"];
+          const CARD = ["VISA", "MASTERCARD"];
           
           const selectedMethod = args.method.toUpperCase();
           const paymentAmount = args.amount;
 
           // EWallet
-          if (["GCASH", "PAYMAYA"].includes(selectedMethod)) {
+          if (EWALLET.includes(selectedMethod)) {
             const data: PaymentRequestParameters = {
               "amount" : paymentAmount,
               "paymentMethod" : {
@@ -41,6 +68,39 @@ export const resolvers = {
             console.log(response);
           };
 
+          if (BANK.includes(selectedMethod)) {
+            // Check if payment method id exists
+            const paymentMethod = await context.prismaReplica.paymentMethods.findFirst({
+              where: {
+                accountId: user.id,
+                channelCode: selectedMethod
+              }
+            });
+
+            if (!paymentMethod) {
+              return {
+                success: false,
+                message: "Payment method not found"
+              };
+            };
+
+            const data: PaymentRequestParameters = {
+              "amount" : 1500,
+              "metadata" : {
+                "sku" : "example-sku-1234"
+              },
+              "paymentMethodId" : "pm-9685a196-81e9-4c73-8d62-97df5aab2762",
+              "currency" : "PHP",
+              "referenceId" : "example-ref-1234"
+            };
+
+            const response: PaymentRequest = await xenditPaymentRequestClient.createPaymentRequest({
+              data
+            });
+
+            console.log(response);
+          };
+
           return {
             success: true,
             message: "Payment created successfully"
@@ -49,7 +109,7 @@ export const resolvers = {
         } catch(err: any) {
           return {
             success: false,
-            message: JSON.stringify(err)
+            message: JSON.stringify(err) // Temporary stringify error
           };
         };
     }
