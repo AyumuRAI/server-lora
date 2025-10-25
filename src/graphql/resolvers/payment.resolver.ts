8fbc12949a15028c3696bca6232266a8dc3168b9
Ayumu-RAI <rai.ayumucueva@gmail.com>
2025-10-25 17:15:28 +0800

    Added a functional payment logic for all payment options (Banks, Cards, and Ewallets)

--- src/graphql/resolvers/payment.resolver.ts
+++ src/graphql/resolvers/payment.resolver.ts
@@ -1,22 +1,26 @@
 import { Context } from "@lib/context";
 import { User, Card, BillingInfo } from "@lib/types";
 import { paymongo } from "@lib/paymongoClient";
+import { GraphQLJSON } from "graphql-type-json";
 
 export const resolvers = {
+  // Apply custom scalar
+  JSON: GraphQLJSON,
+
   Query: {
     _any: async (_any: any, args: {}, context: Context) => {
       return "";
     }
   },
   Mutation: {
-    createPayment: async (_any: any, args: { method: string, amount: number, card: Card, billing: BillingInfo }, context: Context) => {
+    createPayment: async (_any: any, args: { method: string, amount: number, card?: Card | null, billing?: BillingInfo | null, loanId: number }, context: Context) => {
         try {
-          // if (!context.user) {
-          //   return {
-          //     success: false,
-          //     message: "Unauthorized"
-          //   };
-          // };
+          if (!context.user) {
+            return {
+              success: false,
+              message: "Unauthorized"
+            };
+          };
 
           /*
             Payment options:
@@ -33,37 +37,44 @@ export const resolvers = {
             - MASTERCARD
           */
 
-          // const user = context.user as User;
+          const user = context.user as User;
           const EWALLET = ["gcash", "paymaya"];
           const BANK = ["bpi", "bdo", "metrobank", "landbank", "unionbank"];
           const CARD = ["visa", "mastercard"];
+          // const DOB = ["bpi", "unionbank"];
+          const DOB = ["test_bank_one", "test_bank_two"]; // FOR TESTING; ONE = BPI, TWO = UNIONBANK
+          const BRANKAS = ["bdo", "metrobank", "landbank"];
           
           const selectedMethod = args.method.toLowerCase();
           const paymentAmount = args.amount * 100; // Convert to centavos for paymongo
 
+          // Check if PAYMONGO is in sandbox mode
+          const isSandbox = process.env.PAYMONGO_MODE! === "sandbox" ? true : false;
+
+          let paymentIntentData: any;
+          let paymentMethodData;
+          let nextActionUrls = null;
+          let status = "Pending";
+
           // EWallet
           if (EWALLET.includes(selectedMethod)) {
-            const source = await paymongo.post("/sources", {
+            paymentIntentData = {
               data: {
                 attributes: {
-                  type: selectedMethod,
                   amount: paymentAmount,
                   currency: "PHP",
-                  redirect: {
-                    // example redirect url only
-                    success: "https://google.com", 
-                    failed: "https://google.com"
-                  }
+                  payment_method_allowed: ["gcash", "paymaya"]
                 }
               }
-            });
+            };
 
-            const response = source.data.data;
-            const checkoutUrl = response.attributes.checkout_url;
-            const type = "EWALLET";
-            const status = "PENDING";
-
-            console.log(response);
+            paymentMethodData = {
+              data: {
+                attributes: {
+                  type: selectedMethod
+                }
+              }
+            };
           };
 
           // Card
@@ -75,7 +86,17 @@ export const resolvers = {
               };
             };
 
-            const paymentIntent = await paymongo.post("/payment_intents", {
+            // Current yeat
+            const currentYear = parseInt(new Date().getFullYear().toString().slice(2));
+
+            if (args.card.exp_month < 1 || args.card.exp_month > 12 || args.card.exp_year < currentYear) {
+              return {
+                success: false,
+                message: "Invalid expiration date"
+              };
+            };
+
+            paymentIntentData = {
               data: {
                 attributes: {
                   amount: paymentAmount,
@@ -88,17 +109,9 @@ export const resolvers = {
                   }
                 }
               }
-            });
+            };
 
-            const response = paymentIntent.data.data;
-            const clientKey = response.attributes.client_key;
-            const intentId = response.id;
-            const type = "CARD";
-            const status = "REQUIRES_PAYMENT_METHOD";
-
-            // Next Step
-            // Create a payment method
-            const paymentMethod = await paymongo.post("/payment_methods", {
+            paymentMethodData = {
               data: {
                 attributes: {
                   details: {
@@ -118,65 +131,93 @@ export const resolvers = {
                   type: "card"
                 }
               }
-            });
+            };
 
-            const responsePaymentMethod = paymentMethod.data.data;
-            const paymentMethodId = responsePaymentMethod.id;
-
-
-            // Next Step To Charge User
-            const paymentIntentAttach = await paymongo.post(`/payment_intents/${intentId}/attach`, {
-              data: {
-                attributes: {
-                  payment_method: paymentMethodId,
-                  return_url: "https:/google.com" // Example redirect url
-                }
-              }
-            });
-
-            const responsePaymentIntentAttach = paymentIntentAttach.data.data;
-            const statusAttach = responsePaymentIntentAttach.attributes.status;
-
-            // console.log(response);
-            // console.log(responsePaymentMethod);
-            // console.log(responsePaymentIntentAttach);
+            if (isSandbox) {
+              // Defaults to VISA in sandbox, any card number in testing mode will NOT work
+              paymentMethodData.data.attributes.details.card_number = "4343434343434345";
+            };
           };
 
           // Banks
           if (BANK.includes(selectedMethod)) {
-            // TO DO: This is not working code at the moment
-            const source = await paymongo.post("/sources", {
+            paymentIntentData = {
               data: {
                 attributes: {
-                  type: selectedMethod,
                   amount: paymentAmount,
                   currency: "PHP",
-                  redirect: {
-                    // example redirect url only
-                    success: "https://google.com",
-                    failed: "https://google.com"
-                  },
-                  // Required for banks request
-                  billing: {
-                    name: args.billing.name,
-                    email: args.billing.email,
-                    phone: args.billing.phone
+                  payment_method_allowed: ["dob"]
+                }
+              }
+            };
+
+            paymentMethodData = {
+              data: {
+                attributes: {
+                  type: "dob",
+                  details: {
+                    bank_code: selectedMethod
                   }
                 }
               }
-            });
+            };
 
-            const response = source.data.data;
-            const checkoutUrl = response.attributes.checkout_url;
-            const type = "BANK";
-            const status = "PENDING";
+            if(BRANKAS.includes(selectedMethod) && isSandbox) {
+              /*
+                This will defaults to BPI in sandbox mode for non BPI and UNIONBANK
+                Reason: only test_bank_one and test_bank_two are available
+                for paymongo api sandbox, if you use the real bank codes, it will throw an error
+                due to not being allowed to use it on sandbox mode, unless you configure your
+                paymongo api account to allow real bank codes.
+              */
+              paymentMethodData.data.attributes.details.bank_code = "test_bank_one";
+            };
 
-            console.log(response);
+            if (BRANKAS.includes(selectedMethod) && !isSandbox) {
+              paymentIntentData.data.attributes.payment_method_allowed = ["brankas"];
+              paymentMethodData.data.attributes.type = "brankas";
+            };
+
+            if (["bpi", "unionbank"].includes(selectedMethod)) {
+              paymentMethodData.data.attributes.details.bank_code = `${selectedMethod === "bpi" ? DOB[0] : DOB[1]}`;
+            };
+          };
+
+          // Add custom metadata
+          paymentIntentData.data.attributes.metadata = {
+            userId: user.id,
+            loanId: args.loanId.toString()
+          };
+
+          // Main Operation
+          const paymentIntent = await paymongo.post("/payment_intents", paymentIntentData);
+          const intentId = paymentIntent.data.data.id;
+
+          const paymentMethod = await paymongo.post("/payment_methods", paymentMethodData);
+          const paymentMethodId = paymentMethod.data.data.id;
+
+          const paymentIntentAttach = await paymongo.post(`/payment_intents/${intentId}/attach`, {
+            data: {
+              attributes: {
+                payment_method: paymentMethodId,
+                return_url: "https://google.com" // Example only
+              }
+            }
+          });
+
+          // For EWallet and Banks only
+          if (BANK.includes(selectedMethod) || EWALLET.includes(selectedMethod)) {
+            nextActionUrls = paymentIntentAttach.data.data.attributes.next_action;
+          } else {
+            // For Cards
+            status = paymentIntentAttach.data.data.attributes.status;
           };
 
           return {
             success: true,
-            message: "Payment created successfully"
+            message: "Payment created successfully",
+            nextActionUrls,
+            status
           };
 
         } catch(err: any) {
